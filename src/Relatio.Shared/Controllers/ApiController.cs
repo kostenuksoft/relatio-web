6096842201675337afd57f63a6376d661194688f
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2026-02-21 00:54:05 +0200

    chore(shared): add ApiController base class with error handling

---
+++ src/Relatio.Shared/Controllers/ApiController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Http;
+using ErrorOr;
+
+namespace Relatio.Shared.Controllers;
+
+[ApiController]
+public abstract class ApiController : ControllerBase
+{
+    protected IActionResult HandleErrors(List<Error> errors)
+    {
+        if (errors.Count == 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(e => e.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Problem(errors.First());
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+            ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: error.Code, detail: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(error.Code, error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
+    }
+}
